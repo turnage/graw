e45703f7e88ae3230d32050ce6a1a204f4da2b8d
Payton Turnage <paytonturnage@gmail.com>
2015-07-11 23:27:43 -0700

    Adds example file.
    Examples package shows how to get an agent and make an api call from it.

---
+++ examples/examples.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/paytonturnage/graw"
+)
+
+func main() {
+	agent, err := graw.NewAgentFromFile("useragent.protobuf")
+	if err != nil {
+		fmt.Printf("Failed to make agent: %v", err)
+		os.Exit(-1)
+	}
+
+	redditor, err := agent.Me()
+	if err != nil {
+		fmt.Printf("Failed to get own account: %v", err)
+		os.Exit(-1)
+	}
+
+	fmt.Printf("Account: \n%s", redditor.String())
+}
