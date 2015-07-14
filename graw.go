81e0b242d3d3604b8f7cd7bc48a565f3fba04c91
Payton Turnage <paytonturnage@gmail.com>
2015-07-13 22:04:50 -0700

    Enforces reddit's query limit.

--- graw.go
+++ graw.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/paytonturnage/graw/data"
@@ -17,6 +18,9 @@ const (
 	authURL = "https://www.reddit.com/api/v1/access_token"
 	// baseURL is the base url for all api calls.
 	baseURL = "https://oauth.reddit.com/api"
+	// maxQueriesPerWindow is the amount of queries allowed per minute according
+	// to the rules of the reddit api.
+	maxQueriesPerWindow = 60
 )
 
 // Reddit api call urls.
@@ -31,6 +35,10 @@ const (
 type Graw struct {
 	// client manages the Graw's connection to reddit.
 	client *nface.Client
+	// windowStart is the start of Graw's query-limit window.
+	windowStart time.Time
+	// queriesInWindow is the number of queries made in the active window.
+	queriesInWindow int
 }
 
 // NewGraw returns an graw ready to use with reddit. An error is returned
@@ -53,24 +61,44 @@ func NewGrawFromFile(filename string) (*Graw, error) {
 }
 
 // Me wraps /v1/me. See https://www.reddit.com/dev/api#GET_api_v1_me
-func (a *Graw) Me() (*data.Account, error) {
+func (g *Graw) Me() (*data.Account, error) {
 	resp := &data.Account{}
-	err := a.client.Do(&nface.Request{
-		Action: nface.GET,
-		URL:    meURL,
-	}, resp)
+	err := g.do(&nface.Request{Action: nface.GET, URL: meURL}, resp)
 	return resp, err
 }
 
 // MeKarma wraps /v1/me/karma. See
 // https://www.reddit.com/dev/api#GET_api_v1_me_karma
-func (a *Graw) MeKarma() ([]*data.SubredditKarma, error) {
+func (g *Graw) MeKarma() ([]*data.SubredditKarma, error) {
 	resp := &data.KarmaList{}
-	err := a.client.Do(&nface.Request{
-		Action: nface.GET,
-		URL:    meKarmaURL,
-	}, resp)
+	err := g.do(&nface.Request{Action: nface.GET, URL: meKarmaURL}, resp)
 	return resp.GetData(), err
+}
+
+// do executes a request using the Graw's client, and abides the query limit.
+func (g *Graw) do(r *nface.Request, resp interface{}) error {
+	g.clearWindowByBlocking()
+	return g.client.Do(r, resp)
+}
+
+// clearWindowByBlocking keeps track of the Graw's query limit and blocks until
+// it is allowed to make new queries if it reaches it.
+func (g *Graw) clearWindowByBlocking() {
+	timeInWindow := time.Now().Sub(g.windowStart)
+	if timeInWindow < time.Minute {
+		if g.queriesInWindow >= maxQueriesPerWindow {
+			time.Sleep(time.Minute - timeInWindow)
+			g.resetWindow()
+		}
+		g.queriesInWindow++
+	} else {
+		g.resetWindow()
+	}
+}
+
+func (g *Graw) resetWindow() {
+	g.windowStart = time.Now()
+	g.queriesInWindow = 0
 }
 
 // newGrawFromUserAgent returns a new Graw derived from the identity in
