0ea7be1f0766902788f652c1d672a2782190775c
Payton Turnage <paytonturnage@gmail.com>
2015-08-12 22:18:55 -0400

    Give bots access to engine instead of user.
    This is more flexible. If the bot wants to change how the engine generates
    events (e.g. add a new watched subreddit, post, or alarm) it can do that now.
    
    Instead of Triggers, the engine will poll for events itself. The layer of
    indirection was unnecessary.

--- user.go
+++
@@ -1,6 +0,0 @@
-package graw
-
-// User defines behaviors of a Reddit user account, which bots will have access
-// to. These behaviors are not expected to follow through immediately; they will
-// be put in queue and executed as soon as possible.
-type User interface {}
