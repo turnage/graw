f864397d3f6cc99eecbfd9b3f1b44be4e136868b
Payton Turnage <paytonturnage@gmail.com>
2015-08-16 00:24:23 -0400

    Exports Client implementations.
    Former-commit-id: 68e8cdfa69e7ff816e77ddd9b827616cea77977e

--- bot/internal/client/client.go
+++ bot/internal/client/client.go
@@ -2,6 +2,7 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -10,4 +11,28 @@ type Client interface {
 	// Do executes a request and manages authenticating with and identifying
 	// to the server.
 	Do(r *http.Request) (*http.Response, error)
+}
+
+// New returns a new Client from a user agent file. This user agent file is
+// expected to have been generated with "graw grant".
+func New(filename string) (Client, error) {
+	agent, err := load(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if agent.GetRefreshToken() == "" {
+		return nil, fmt.Errorf("no refresh token; see graw grant")
+	}
+
+	return &client{cli: build(
+		agent.GetUserAgent(),
+		agent.GetClientId(),
+		agent.GetRefreshToken(),
+	)}, nil
+}
+
+// NewMock returns a mock client that returns canned values for calls to Do().
+func NewMock(r *http.Response, err error) Client {
+	return &mockClient{r, err}
 }
