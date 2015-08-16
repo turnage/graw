5c51b506c7582a0d2f31d0854d91c32e6ed8aeb6
Payton Turnage <paytonturnage@gmail.com>
2015-08-15 20:46:14 -0400

    Refactor load of user agents.
    Reduce code paths in functions and cover all of them.
    
    
    Former-commit-id: 4a341c7a1a053de51ad9e7a1769fe9d868ca4592

--- client/loader.go
+++ client/loader.go
@@ -10,16 +10,11 @@ import (
 
 // load reads a user agent from a protobuffer file and returns it.
 func load(filename string) (*redditproto.UserAgent, error) {
-	agentBytes, err := ioutil.ReadFile(filename)
+	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	buffer := bytes.NewBuffer(agentBytes)
 	agent := &redditproto.UserAgent{}
-	if err := proto.UnmarshalText(buffer.String(), agent); err != nil {
-		return nil, err
-	}
-
-	return agent, nil
+	return agent, proto.UnmarshalText(bytes.NewBuffer(buf).String(), agent)
 }
