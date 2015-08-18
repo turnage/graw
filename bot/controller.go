4bc54b4c27c1eee974dd3418c2c1a8b34c7e10bb
Payton Turnage <paytonturnage@gmail.com>
2015-08-17 21:23:43 -0400

    Implement reasonable bot controller api.
    This implements the methods necessary for a read-only bot.
    
    
    Former-commit-id: a8827487c32efb9b4043e348dbef192baa5cce34

--- bot/controller.go
+++ bot/controller.go
@@ -1,8 +1,16 @@
 package bot
 
+import (
+	"time"
+)
+
 // Controller defines the interface for bots to interact with the engine. These
 // methods are requests to the engine to perform actions on behalf of the bot,
 // when it decides it is time.
-type Controller interface{}
-
-type controller struct{}
+type Controller interface {
+	// SetAlarm configures a delayed event. The name will be passed to the
+	// bot's Alarm() method when the delay expires.
+	SetAlarm(delay time.Duration, name string)
+	// Stop stops the engine execution.
+	Stop()
+}
