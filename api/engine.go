97477b438ee2540759c805a1775fd76da5336349
Payton Turnage <paytonturnage@gmail.com>
2015-08-25 23:10:39 +0000

    Reorganize package for godoc.

---
+++ api/engine.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"github.com/turnage/redditproto"
+)
+
+// Engine exposes functions of the Engine to the bot.
+type Engine interface {
+	// Reply posts a reply to something on reddit. The behavior depends on
+	// what is being replied to. For
+	//
+	//   messages, this sends a private message reply.
+	//   posts, this posts a top level comment.
+	//   comments, this posts a comment reply.
+	//
+	// Use GetName() on the parent post, message, or comment to find its
+	// name.
+	Reply(parentName, text string) error
+
+	// SendMessage sends a private message to a user.
+	SendMessage(user, subject, text string) error
+
+	// SelfPost makes a text post to a subreddit.
+	SelfPost(subreddit, title, text string) error
+
+	// LinkPost makes a link post to a subreddit.
+	LinkPost(subreddit, title, url string) error
+
+	// DigestThread returns a post with a parsed comment tree. Call
+	// GetComments() on the returned Link for a slice of top level comments,
+	// and GetReplyTree() on Comments for their replies.
+	//
+	// Use GetName() on a Link to find its name.
+	DigestThread(threadName string) (*redditproto.Link, error)
+
+	// Stop stops the engine. If it implemented it, the bot's TearDown
+	// method will be called.
+	Stop()
+}
