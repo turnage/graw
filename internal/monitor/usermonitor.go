044213de2ad34efeafb3f70075d696423126bb22
Payton Turnage <paytonturnage@gmail.com>
2015-10-21 22:24:15 -0400

    Implements user monitoring.
    Former-commit-id: 6df6e710bc1b3a1188166bac1bec8a6d2327a0c2

---
+++ internal/monitor/usermonitor.go
@@ -0,0 +1,57 @@
+package monitor
+
+import (
+	"github.com/turnage/graw/api"
+	"github.com/turnage/graw/internal/monitor/internal/scanner"
+	"github.com/turnage/graw/internal/operator"
+)
+
+// userMonitor monitors a user for new posts and comments.
+type userMonitor struct {
+	// userScanner is the scanner userMonitor uses to get updates from
+	// the user it monitors.
+	userScanner scanner.Scanner
+	// userHandler is the handler UserMonitor will send new posts and
+	// comments by the watched user to.
+	userHandler api.UserHandler
+}
+
+// UserMonitor returns a user monitor for the requested user.
+func UserMonitor(
+	op operator.Operator,
+	bot interface{},
+	user string,
+) Monitor {
+	userHandler, ok := bot.(api.UserHandler)
+	if !ok {
+		return nil
+	}
+
+	if user == "" {
+		return nil
+	}
+
+	return &userMonitor{
+		userScanner: scanner.NewUserScanner(user, op),
+		userHandler: userHandler,
+	}
+}
+
+// Update polls for new user activity and sends the content to Bot when it is
+// found.
+func (p *userMonitor) Update() error {
+	posts, comments, err := p.userScanner.Scan()
+	if err != nil {
+		return err
+	}
+
+	for _, post := range posts {
+		go p.userHandler.UserPost(post)
+	}
+
+	for _, comment := range comments {
+		go p.userHandler.UserComment(comment)
+	}
+
+	return nil
+}
---
+++ internal/monitor/usermonitor_test.go
@@ -0,0 +1,104 @@
+package monitor
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/turnage/graw/internal/monitor/internal/scanner"
+	"github.com/turnage/graw/internal/operator"
+	"github.com/turnage/redditproto"
+)
+
+type mockUserHandler struct {
+	postCalls    int
+	commentCalls int
+}
+
+func (m *mockUserHandler) UserPost(post *redditproto.Link) {
+	m.postCalls++
+}
+
+func (m *mockUserHandler) UserComment(comment *redditproto.Comment) {
+	m.commentCalls++
+}
+
+func TestUserMonitor(t *testing.T) {
+	if um := UserMonitor(
+		&operator.MockOperator{},
+		&mockNoHandler{},
+		"user",
+	); um != nil {
+		t.Errorf("got %v; wanted nil", um)
+	}
+
+	if um := UserMonitor(
+		&operator.MockOperator{},
+		&mockUserHandler{},
+		"",
+	); um != nil {
+		t.Errorf("got %v; wanted nil", um)
+	}
+
+	um := UserMonitor(
+		&operator.MockOperator{},
+		&mockUserHandler{},
+		"user",
+	).(*userMonitor)
+	if um.userHandler == nil {
+		t.Errorf("wanted userHandler set")
+	}
+}
+
+func TestUserMonitorUpdate(t *testing.T) {
+	um := UserMonitor(
+		&operator.MockOperator{},
+		&mockUserHandler{},
+		"user",
+	)
+	mon := um.(*userMonitor)
+	mon.userScanner = &scanner.MockScanner{
+		ScanErr: fmt.Errorf("an error"),
+	}
+	if err := um.Update(); err == nil {
+		t.Errorf("wanted error for request failure")
+	}
+
+	bot := &mockUserHandler{}
+	um = UserMonitor(
+		&operator.MockOperator{},
+		bot,
+		"user",
+	)
+	mon = um.(*userMonitor)
+	mon.userScanner = &scanner.MockScanner{
+		ScanLinksReturn: []*redditproto.Link{
+			&redditproto.Link{},
+			&redditproto.Link{},
+		},
+		ScanCommentsReturn: []*redditproto.Comment{
+			&redditproto.Comment{},
+			&redditproto.Comment{},
+		},
+	}
+	if err := um.Update(); err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	// Allow bot goroutines to work.
+	time.Sleep(time.Second)
+
+	if bot.postCalls != 2 {
+		t.Errorf(
+			"%d calls were made to mock bot; wanted 2",
+			bot.postCalls,
+		)
+	}
+
+	if bot.commentCalls != 2 {
+		t.Errorf(
+			"%d calls were made to mock bot; wanted 2",
+			bot.commentCalls,
+		)
+	}
+}
