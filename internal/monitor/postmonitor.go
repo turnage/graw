2c50f186d43946d92079cfd79c52810f85343b61
Payton Turnage <paytonturnage@gmail.com>
2015-10-30 16:34:18 -0400

    [No build] Rewrite user and post monitors w/base.
    This rewrites the user and post monitors with the shared monitor base struct.
    The two of them were almost completely identical, and no their redundant code
    isn't wasting my hard drive space. Actually I guess since it's a git repo it
    still is. Oh well.

--- internal/monitor/postmonitor.go
+++ internal/monitor/postmonitor.go
@@ -1,49 +1,39 @@
 package monitor
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/turnage/graw/api"
-	"github.com/turnage/graw/internal/monitor/internal/scanner"
+	"github.com/turnage/graw/internal/monitor/internal/handlers"
 	"github.com/turnage/graw/internal/operator"
 )
 
 // postMonitor monitors subreddits for new posts, and sends them to its handler.
 type postMonitor struct {
-	// postScanner is the scanner postMonitor uses to get updates from
-	// subreddits it monitors.
-	postScanner scanner.Scanner
-	// postHandler is the handler PostMonitor will send new posts to.
-	postHandler api.PostHandler
+	base
 }
 
 // PostMonitor returns a post monitor for the requested subreddits, using bot
 // to handle new posts it finds.
 func PostMonitor(
 	op operator.Operator,
-	bot api.PostHandler,
+	bot handlers.PostHandler,
 	subreddits []string,
-) Monitor {
-	return &postMonitor{
-		postScanner: scanner.NewPostScanner(
-			fmt.Sprintf("%s", strings.Join(subreddits, "+")),
-			op,
-		),
-		postHandler: bot,
-	}
-}
-
-// Update polls for new posts and sends them to Bot when they are found.
-func (p *postMonitor) Update() error {
-	posts, _, err := p.postScanner.Scan()
-	if err != nil {
-		return err
+	dir Direction,
+) (Monitor, error) {
+	p := &postMonitor{
+		base: base{
+			handlePost: bot.Post,
+			dir:        dir,
+			path:       "/r/" + strings.Join(subreddits, "+"),
+			tip:        []string{""},
+		},
 	}
 
-	for _, post := range posts {
-		go p.postHandler.Post(post)
+	if dir == Forward {
+		if err := p.sync(op); err != nil {
+			return nil, err
+		}
 	}
 
-	return nil
+	return p, nil
 }
--- internal/monitor/usermonitor_test.go
+++ internal/monitor/usermonitor_test.go
@@ -1,89 +1,44 @@
 package monitor
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
-	"github.com/turnage/graw/internal/monitor/internal/scanner"
 	"github.com/turnage/graw/internal/operator"
 	"github.com/turnage/redditproto"
 )
 
-type mockUserHandler struct {
-	postCalls    int
-	commentCalls int
-}
+type mockUserHandler struct{}
 
-func (m *mockUserHandler) UserPost(post *redditproto.Link) {
-	m.postCalls++
-}
+func (m *mockUserHandler) UserPost(post *redditproto.Link) {}
 
-func (m *mockUserHandler) UserComment(comment *redditproto.Comment) {
-	m.commentCalls++
-}
+func (m *mockUserHandler) UserComment(comment *redditproto.Comment) {}
 
 func TestUserMonitor(t *testing.T) {
-	um := UserMonitor(
-		&operator.MockOperator{},
-		&mockUserHandler{},
-		"user",
-	).(*userMonitor)
-	if um.userHandler == nil {
-		t.Errorf("wanted userHandler set")
-	}
-}
-
-func TestUserMonitorUpdate(t *testing.T) {
-	um := UserMonitor(
-		&operator.MockOperator{},
-		&mockUserHandler{},
-		"user",
-	)
-	mon := um.(*userMonitor)
-	mon.userScanner = &scanner.MockScanner{
-		ScanErr: fmt.Errorf("an error"),
-	}
-	if err := um.Update(); err == nil {
-		t.Errorf("wanted error for request failure")
-	}
-
-	bot := &mockUserHandler{}
-	um = UserMonitor(
-		&operator.MockOperator{},
-		bot,
-		"user",
-	)
-	mon = um.(*userMonitor)
-	mon.userScanner = &scanner.MockScanner{
-		ScanLinksReturn: []*redditproto.Link{
-			&redditproto.Link{},
-			&redditproto.Link{},
+	mon, err := UserMonitor(
+		&operator.MockOperator{
+			ScrapeLinksReturn: []*redditproto.Link{
+				&redditproto.Link{},
+			},
 		},
-		ScanCommentsReturn: []*redditproto.Comment{
-			&redditproto.Comment{},
-			&redditproto.Comment{},
-		},
-	}
-	if err := um.Update(); err != nil {
-		t.Fatalf("error: %v", err)
-	}
-
-	for i := 0; i < 100 && bot.postCalls+bot.commentCalls < 4; i++ {
-		time.Sleep(10 * time.Millisecond)
+		&mockUserHandler{},
+		"rob",
+		Forward,
+	)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	if bot.postCalls != 2 {
-		t.Errorf(
-			"%d calls were made to mock bot; wanted 2",
-			bot.postCalls,
-		)
+	u := mon.(*userMonitor)
+	if u.handlePost == nil {
+		t.Errorf("wanted post handler set")
 	}
-
-	if bot.commentCalls != 2 {
-		t.Errorf(
-			"%d calls were made to mock bot; wanted 2",
-			bot.commentCalls,
-		)
+	if u.handleComment == nil {
+		t.Errorf("wanted comment handler set")
+	}
+	if u.path != "/user/rob" {
+		t.Errorf("got %s; wanted /user/rob", u.path)
+	}
+	if u.dir != Forward {
+		t.Errorf("got %d; wanted %d (Forward)", u.dir, Forward)
 	}
 }
