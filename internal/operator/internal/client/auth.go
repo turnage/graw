f5ea7ea829b3544d0f2650f1c40be728bd5b92d1
Payton Turnage <paytonturnage@gmail.com>
2015-11-06 15:40:23 -0500

    Add test hooks to operator and client.
    These will allow end to end tests to run against a different domain, and force
    the oauth2 package to respect the local test instance of Reddit's self-signed
    certificate.

--- internal/operator/internal/client/auth.go
+++ internal/operator/internal/client/auth.go
@@ -1,22 +1,26 @@
 package client
 
 import (
+	"crypto/tls"
 	"net/http"
 
+	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
 
-const (
-	// tokenURL is the url of reddit's oauth2 authorization service.
-	tokenURL = "https://www.reddit.com/api/v1/access_token"
+var (
+	// TokenURL is the url of reddit's oauth2 authorization service.
+	TokenURL = "https://www.reddit.com/api/v1/access_token"
+	// TestMode is a flag that indicates whether graw is in test mode.
+	TestMode = false
 )
 
-// build returns an http clientt that has built in oauth2 handling.
+// build returns an http client that has built in oauth2 handling.
 func build(id, secret, user, pass string) (*http.Client, *oauth2.Token, error) {
 	cfg := &oauth2.Config{
 		ClientID:     id,
 		ClientSecret: secret,
-		Endpoint:     oauth2.Endpoint{TokenURL: tokenURL},
+		Endpoint:     oauth2.Endpoint{TokenURL: TokenURL},
 		Scopes: []string{
 			"identity",
 			"read",
@@ -25,6 +29,56 @@ func build(id, secret, user, pass string) (*http.Client, *oauth2.Token, error) {
 			"history",
 		},
 	}
+
+	if TestMode {
+		return buildTestClient(cfg, user, pass)
+	}
+
+	return buildProductionClient(cfg, user, pass)
+}
+
+// buildProductionClient returns a client equipped to make requests to
+// a production Reddit instance.
+func buildProductionClient(
+	cfg *oauth2.Config,
+	user,
+	pass string,
+) (
+	*http.Client,
+	*oauth2.Token,
+	error,
+) {
 	token, err := cfg.PasswordCredentialsToken(oauth2.NoContext, user, pass)
 	return cfg.Client(oauth2.NoContext, token), token, err
+}
+
+// buildTestClient returns a client equipped to make requests to a production
+// Reddit instance.
+func buildTestClient(
+	cfg *oauth2.Config,
+	user,
+	pass string,
+) (
+	*http.Client,
+	*oauth2.Token,
+	error,
+) {
+	naiveTransport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	naiveContext := context.WithValue(
+		oauth2.NoContext,
+		oauth2.HTTPClient,
+		&http.Client{
+			Transport: naiveTransport,
+		},
+	)
+
+	token, err := cfg.PasswordCredentialsToken(naiveContext, user, pass)
+	client := cfg.Client(oauth2.NoContext, token)
+	client.Transport.(*oauth2.Transport).Base = naiveTransport
+	return client, token, err
 }
