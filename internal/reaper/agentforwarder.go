8de495d794cff1537a974d52ae5f8421bb5b604c
Payton Turnage <paytonturnage@gmail.com>
2016-10-19 22:15:02 -0400

    Implement reaper package.
    This package will replace the client package in graw version 1. I rewrote it
    because client had become confusing and difficult to modify. The name was also
    confusing because http.Client. Reaper sounds pretty cool.
    
    
    Former-commit-id: ae1b006814e96d1b15f18bcd521c25afd1da02f3

---
+++ internal/reaper/agentforwarder.go
@@ -0,0 +1,18 @@
+package reaper
+
+import (
+	"net/http"
+)
+
+// agentForward forwards a user agent in all requests made by the Transport.
+type agentForwarder struct {
+	http.Transport
+	agent string
+}
+
+// RoundTrip sets a predefined agent in the request and then forwards it to the
+// default RountTrip implementation.
+func (a *agentForwarder) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("User-Agent", a.agent)
+	return a.Transport.RoundTrip(r)
+}
---
+++ internal/reaper/reaper.go
@@ -0,0 +1,93 @@
+// Package reaper reaps http Requests which require OAuth2 authorization.
+package reaper
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+var (
+	PermissionDeniedErr = fmt.Errorf("unauthorized access to endpoint")
+	BusyErr             = fmt.Errorf("service is busy right now")
+	RateLimitErr        = fmt.Errorf("service rate limiting requests")
+)
+
+// Config holds all the information needed to define Reaper behavior, such as
+// who the reaper will identify as externally and where to authorize.
+type Config struct {
+	// Agent is the user agent set in all requests made by the Reaper.
+	Agent string
+
+	// ID and Secret are used to claim an OAuth2 grant the users are
+	// previously authorized.
+	ID     string
+	Secret string
+
+	// Username and Password are used to authorize with the endpoint.
+	Username string
+	Password string
+
+	// TokenURL is the url of the token request location for OAuth2.
+	TokenURL string
+}
+
+// Reaper executes http Requests and invisibly handles OAuth2 authorization.
+type Reaper interface {
+	Reap(r *http.Request) (string, error)
+}
+
+type reaper struct {
+	cli *http.Client
+}
+
+func (r *reaper) Reap(req *http.Request) (string, error) {
+	resp, err := r.cli.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+	case http.StatusForbidden:
+		return "", PermissionDeniedErr
+	case http.StatusServiceUnavailable:
+		return "", BusyErr
+	case http.StatusTooManyRequests:
+		return "", RateLimitErr
+	default:
+		return "", fmt.Errorf("bad response code: %d", resp.StatusCode)
+	}
+
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(resp.Body); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
+
+// New returns a new Reaper using the given user to reap requests.
+func New(c Config) (Reaper, error) {
+	transport := &agentForwarder{agent: c.Agent}
+	cli := &http.Client{Transport: transport}
+	ctx := context.WithValue(oauth2.NoContext, oauth2.HTTPClient, cli)
+	cfg := &oauth2.Config{
+		ClientID:     c.ID,
+		ClientSecret: c.Secret,
+		Endpoint:     oauth2.Endpoint{TokenURL: c.TokenURL},
+		Scopes: []string{
+			"identity",
+			"read",
+			"privatemessages",
+			"submit",
+			"history",
+		},
+	}
+
+	token, err := cfg.PasswordCredentialsToken(ctx, c.Username, c.Password)
+	return &reaper{cfg.Client(ctx, token)}, err
+}
