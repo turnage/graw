7bfd6e7bfa6967a3bffec54691422b753ba74783
Payton Turnage <paytonturnage@gmail.com>
2016-11-03 01:01:21 -0400

    Begin tearing down monitor package.
    Because why not? I'm tearing everything else down.
    
    
    Former-commit-id: 3a04d304cf8461d4fa36b950b11e688bd5ddba17

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -6,12 +6,8 @@ import (
 	"fmt"
 	"sort"
 
-	"github.com/turnage/redditproto"
+	"github.com/turnage/graw/internal/data"
 )
-
-type commentHandler func(*redditproto.Comment)
-type postHandler func(*redditproto.Link)
-type messageHandler func(*redditproto.Message)
 
 const (
 	// The blank threshold is the amount of updates returning 0 new
@@ -23,52 +19,22 @@ const (
 	maxTipSize = 10
 )
 
-// Scraper defines a function that takes a Reddit listing path, and a an id of
-// an element within the listing, and returns all the elements following that
-// element. If limit is <= 0, as many elements as possible should be returned.
-type Scraper func(
-	path,
-	tip string,
-	limit int,
-) (
-	[]*redditproto.Link,
-	[]*redditproto.Comment,
-	[]*redditproto.Message,
-	error,
-)
-
-// Prober defines a function type that takes the id of a reddit thing and
-// returns whether it exists.
-type Prober func(id string) (bool, error)
+// PostHandler handles new posts in watched subreddits.
+type PostHandler interface {
+	// Post handles a new post in a watched subreddit.
+	Post(p *data.Post) error
+}
 
 // Monitor defines the controls for a Monitor.
 type Monitor interface {
 	// Update will check for new events, and send them to the Monitor's
 	// handlers.
-	Update(scrape Scraper, probe Prober) error
+	Update() error
 }
 
-// redditThing is an interface for accessing attributes of Reddit types that
-// implement the Redddit "Thing" class.
-type redditThing interface {
-	// GetName returns the fullname of the thing.
-	GetName() string
-	// GetCreatedUtc returns the creation time (Unix time) of the thing.
-	GetCreatedUtc() float64
-}
-
-// base describes the core of any monitor; most monitors will use its fields and
+// monitor describes the core of any monitor; most monitors will use its fields and
 // methods.
-type base struct {
-	// handlePost is the function the monitor uses to handle new posts it
-	// finds.
-	handlePost postHandler
-	// handleComment is the function the monitor uses to handle new comments
-	// it finds.
-	handleComment commentHandler
-	// handleMessage is the function the monitor uses to handle new messages
-	// it finds.
-	handleMessage messageHandler
+type monitor struct {
 	// blanks is the number of. rounds that have turned up 0 new
 	// elements at the listing endpoint.
 	blanks int
@@ -80,12 +46,16 @@ type base struct {
 	// as a reference point (i.e.asks Reddit for posts "after" the tip).
 	tip []string
 	// path is the listing endpoint the monitor monitors. This path is
-	// appended to the reddit base url (e.g./user/robert).
+	// appended to the reddit monitor url (e.g./user/robert).
 	path string
+
+	replyHandler ReplyHandler
+	postHandler  PostHandler
+	userHandler  UserHandler
 }
 
-// baseFromPath provides a monitor base from the listing endpoint.
-func baseFromPath(
+// monitorFromPath provides a monitor monitor from the listing endpoint.
+func monitorFromPath(
 	scrape Scraper,
 	path string,
 	handlePost postHandler,
@@ -96,7 +66,7 @@ func baseFromPath(
 		return nil, fmt.Errorf("no handlers provided for events")
 	}
 
-	b := &base{
+	b := &monitor{
 		handlePost:    handlePost,
 		handleComment: handleComment,
 		handleMessage: handleMessage,
@@ -104,7 +74,7 @@ func baseFromPath(
 		tip:           []string{""},
 	}
 
-	if err := b.sync(scrape); err != nil {
+	if err := m.sync(scrape); err != nil {
 		return nil, err
 	}
 
@@ -113,55 +83,55 @@ func baseFromPath(
 
 // dispatch starts goroutines of the appropriate handler function for all new
 // elements the monitor discovers.
-func (b *base) dispatch(
+func (m *monitor) dispatch(
 	posts []*redditproto.Link,
 	comments []*redditproto.Comment,
 	messages []*redditproto.Message,
 ) {
-	if b.handlePost != nil {
+	if m.handlePost != nil {
 		for _, post := range posts {
-			go b.handlePost(post)
+			go m.handlePost(post)
 		}
 	}
 
-	if b.handleComment != nil {
+	if m.handleComment != nil {
 		for _, comment := range comments {
-			go b.handleComment(comment)
+			go m.handleComment(comment)
 		}
 	}
 
-	if b.handleMessage != nil {
+	if m.handleMessage != nil {
 		for _, message := range messages {
-			go b.handleMessage(message)
+			go m.handleMessage(message)
 		}
 	}
 }
 
 // Update checks for new content at the monitored listing endpoint and forwards
 // new content to the bot for processing.
-func (b *base) Update(scrape Scraper, probe Prober) error {
-	posts, comments, messages, err := scrape(b.path, b.tip[0], -1)
+func (m *monitor) Update(scrape Scraper, probe Prober) error {
+	posts, comments, messages, err := scrape(m.path, m.tip[0], -1)
 	if err != nil {
 		return err
 	}
 
-	b.dispatch(posts, comments, messages)
-	return b.updateTip(posts, comments, messages, probe)
+	m.dispatch(posts, comments, messages)
+	return m.updateTip(posts, comments, messages, probe)
 }
 
 // sync fetches the current tip of a listing endpoint, so that grawbots crawling
 // forward in time don't treat it as a new post, or reprocess it when restarted.
-func (b *base) sync(scrape Scraper) error {
-	posts, messages, comments, err := scrape(b.path, "", 1)
+func (m *monitor) sync(scrape Scraper) error {
+	posts, messages, comments, err := scrape(m.path, "", 1)
 	if err != nil {
 		return err
 	}
 
 	things := merge(posts, messages, comments)
 	if len(things) == 1 {
-		b.tip = []string{things[0].GetName()}
+		m.tip = []string{things[0].GetName()}
 	} else {
-		b.tip = []string{""}
+		m.tip = []string{""}
 	}
 
 	return nil
@@ -170,7 +140,7 @@ func (b *base) sync(scrape Scraper) error {
 //.Tip.s the monitor's list of names from the endpoint listing it
 // uses to keep track of its position in the monitored listing (e.g.a user's
 // page or its position in a subreddit's history).
-func (b *base) updateTip(
+func (m *monitor) updateTip(
 	posts []*redditproto.Link,
 	comments []*redditproto.Comment,
 	messages []*redditproto.Message,
@@ -178,16 +148,16 @@ func (b *base) updateTip(
 ) error {
 	things := merge(posts, comments, messages)
 	if len(things) == 0 {
-		return b.healthCheck(probe)
+		return m.healthCheck(probe)
 	}
 
 	names := make([]string, len(things))
 	for i := 0; i < len(things); i++ {
 		names[i] = things[i].GetName()
 	}
-	b.tip = append(names, b.tip...)
-	if len(b.tip) > maxTipSize {
-		b.tip = b.tip[0:maxTipSize]
+	m.tip = append(names, m.tip...)
+	if len(m.tip) > maxTipSize {
+		m.tip = m.tip[0:maxTipSize]
 	}
 
 	return nil
@@ -195,16 +165,16 @@ func (b *base) updateTip(
 
 // healthCheck checks the health of the tip when nothing is returned from a
 // scrape enough times.
-func (b *base) healthCheck(probe Prober) error {
-	b.blanks++
-	if b.blanks > b.blankThreshold {
-		b.blanks = 0
-		broken, err := b.fixTip(probe)
+func (m *monitor) healthCheck(probe Prober) error {
+	m.blanks++
+	if m.blanks > m.blankThreshold {
+		m.blanks = 0
+		broken, err := m.fixTip(probe)
 		if err != nil {
 			return err
 		}
 		if !broken {
-			b.blankThreshold += blankThreshold
+			m.blankThreshold += blankThreshold
 		}
 	}
 	return nil
@@ -213,14 +183,14 @@ func (b *base) healthCheck(probe Prober) error {
 // fixTip checks that the fullname at the front of the tip is still valid (e.g.
 // not deleted).If it isn't, it shaves the tip.fixTip returns whether the tip
 // was broken.
-func (b *base) fixTip(probe Prober) (bool, error) {
-	exists, err := probe(b.tip[0])
+func (m *monitor) fixTip(probe Prober) (bool, error) {
+	exists, err := probe(m.tip[0])
 	if err != nil {
 		return false, err
 	}
 
 	if exists == false {
-		b.shaveTip()
+		m.shaveTip()
 	}
 
 	return !exists, nil
@@ -228,36 +198,10 @@ func (b *base) fixTip(probe Prober) (bool, error) {
 
 // shaveTip shaves the latest fullname off of the tip, promoting the preceding
 // fullname if there is one or resetting the tip if there isn't.
-func (b *base) shaveTip() {
-	if len(b.tip) <= 1 {
-		b.tip = []string{""}
+func (m *monitor) shaveTip() {
+	if len(m.tip) <= 1 {
+		m.tip = []string{""}
 	} else {
-		b.tip = b.tip[1:]
+		m.tip = m.tip[1:]
 	}
-}
-
-// merge merges elements of multiple listings which implement redditThing into
-// one slice, ordered in creation time. merge assumes all of the listings
-// provided are ordered independently by creation time. The total listing size
-// will very rarely exceed 15 or so.
-func merge(
-	posts []*redditproto.Link,
-	comments []*redditproto.Comment,
-	messages []*redditproto.Message,
-) []redditThing {
-	// Why is handling interface slices so painful?
-	things := []redditThing{}
-	for _, post := range posts {
-		things = append(things, post)
-	}
-	for _, comment := range comments {
-		things = append(things, comment)
-	}
-	for _, message := range messages {
-		things = append(things, message)
-	}
-
-	sort.Sort(byCreationTime{things})
-
-	return things
 }
