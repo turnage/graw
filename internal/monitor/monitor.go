a67b4aeb3885c1f0327ec990fead85dc62efde52
Payton Turnage <paytonturnage@gmail.com>
2016-11-05 20:23:14 -0700

    Rewrite the monitor package.
    The monitor package no longer has disbatch responsibilities.
    
    
    Former-commit-id: a1a483722ee4648fb56b5a67a62bb0d72328b8cc

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -3,39 +3,42 @@
 package monitor
 
 import (
-	"fmt"
-	"sort"
-
-	"github.com/turnage/graw/internal/data"
+	"github.com/turnage/graw/internal/api"
+	"github.com/turnage/graw/internal/reap"
+	"github.com/turnage/graw/internal/rsort"
 )
 
 const (
 	// The blank threshold is the amount of updates returning 0 new
 	// elements in the monitored listing the monitor will tolerate before
 	// suspecting the tip of the listing has been deleted.
-	blankThreshold = 2
+	blankThreshold = 1
 	// maxTipSize is the maximum size of the tip log (number of backup tips
 	// + the current tip).
-	maxTipSize = 10
+	maxTipSize = 20
 )
-
-// PostHandler handles new posts in watched subreddits.
-type PostHandler interface {
-	// Post handles a new post in a watched subreddit.
-	Post(p *data.Post) error
-}
 
 // Monitor defines the controls for a Monitor.
 type Monitor interface {
 	// Update will check for new events, and send them to the Monitor's
 	// handlers.
-	Update() error
+	Update() (reap.Harvest, error)
 }
 
-// monitor describes the core of any monitor; most monitors will use its fields and
-// methods.
+// Config configures a monitor.
+type Config struct {
+	// Path is the path to the listing the monitor watches.
+	Path string
+
+	// Lurker is the api the monitor uses to read Reddit data.
+	Lurker api.Lurker
+
+	// Sorter sorts the monitor's new listing elements.
+	Sorter rsort.Sorter
+}
+
 type monitor struct {
-	// blanks is the number of. rounds that have turned up 0 new
+	// blanks is the number of rounds that have turned up 0 new
 	// elements at the listing endpoint.
 	blanks int
 	// blankThreshold is the number of blanks a monitor will tolerate before
@@ -49,87 +52,49 @@ type monitor struct {
 	// appended to the reddit monitor url (e.g./user/robert).
 	path string
 
-	replyHandler ReplyHandler
-	postHandler  PostHandler
-	userHandler  UserHandler
+	lurker api.Lurker
+	sorter rsort.Sorter
 }
 
-// monitorFromPath provides a monitor monitor from the listing endpoint.
-func monitorFromPath(
-	scrape Scraper,
-	path string,
-	handlePost postHandler,
-	handleComment commentHandler,
-	handleMessage messageHandler,
-) (Monitor, error) {
-	if handlePost == nil && handleComment == nil && handleMessage == nil {
-		return nil, fmt.Errorf("no handlers provided for events")
+// New provides a monitor for the listing endpoint.
+func New(c Config) (Monitor, error) {
+	m := &monitor{
+		blankThreshold: blankThreshold,
+		tip:            []string{""},
+		path:           c.Path,
+		lurker:         c.Lurker,
+		sorter:         c.Sorter,
 	}
 
-	b := &monitor{
-		handlePost:    handlePost,
-		handleComment: handleComment,
-		handleMessage: handleMessage,
-		path:          path,
-		tip:           []string{""},
-	}
-
-	if err := m.sync(scrape); err != nil {
+	if err := m.sync(); err != nil {
 		return nil, err
 	}
 
-	return b, nil
-}
-
-// dispatch starts goroutines of the appropriate handler function for all new
-// elements the monitor discovers.
-func (m *monitor) dispatch(
-	posts []*redditproto.Link,
-	comments []*redditproto.Comment,
-	messages []*redditproto.Message,
-) {
-	if m.handlePost != nil {
-		for _, post := range posts {
-			go m.handlePost(post)
-		}
-	}
-
-	if m.handleComment != nil {
-		for _, comment := range comments {
-			go m.handleComment(comment)
-		}
-	}
-
-	if m.handleMessage != nil {
-		for _, message := range messages {
-			go m.handleMessage(message)
-		}
-	}
+	return m, nil
 }
 
 // Update checks for new content at the monitored listing endpoint and forwards
 // new content to the bot for processing.
-func (m *monitor) Update(scrape Scraper, probe Prober) error {
-	posts, comments, messages, err := scrape(m.path, m.tip[0], -1)
+func (m *monitor) Update() (reap.Harvest, error) {
+	harvest, err := m.lurker.Listing(m.path, m.tip[0])
 	if err != nil {
-		return err
+		return harvest, err
 	}
 
-	m.dispatch(posts, comments, messages)
-	return m.updateTip(posts, comments, messages, probe)
+	return harvest, m.updateTip(harvest)
 }
 
 // sync fetches the current tip of a listing endpoint, so that grawbots crawling
 // forward in time don't treat it as a new post, or reprocess it when restarted.
-func (m *monitor) sync(scrape Scraper) error {
-	posts, messages, comments, err := scrape(m.path, "", 1)
+func (m *monitor) sync() error {
+	harvest, err := m.lurker.Listing(m.path, "")
 	if err != nil {
 		return err
 	}
 
-	things := merge(posts, messages, comments)
-	if len(things) == 1 {
-		m.tip = []string{things[0].GetName()}
+	names := m.sorter.Sort(harvest)
+	if len(names) > 0 {
+		m.tip = names
 	} else {
 		m.tip = []string{""}
 	}
@@ -137,24 +102,17 @@ func (m *monitor) sync(scrape Scraper) error {
 	return nil
 }
 
-//.Tip.s the monitor's list of names from the endpoint listing it
-// uses to keep track of its position in the monitored listing (e.g.a user's
+// updateTip updates the monitor's list of names from the endpoint listing it
+// uses to keep track of its position in the monitored listing (e.g. a user's
 // page or its position in a subreddit's history).
-func (m *monitor) updateTip(
-	posts []*redditproto.Link,
-	comments []*redditproto.Comment,
-	messages []*redditproto.Message,
-	probe Prober,
-) error {
-	things := merge(posts, comments, messages)
-	if len(things) == 0 {
-		return m.healthCheck(probe)
+func (m *monitor) updateTip(h reap.Harvest) error {
+	names := m.sorter.Sort(h)
+	if len(names) == 0 {
+		return m.healthCheck()
+	} else {
+		m.blanks = 0
 	}
 
-	names := make([]string, len(things))
-	for i := 0; i < len(things); i++ {
-		names[i] = things[i].GetName()
-	}
 	m.tip = append(names, m.tip...)
 	if len(m.tip) > maxTipSize {
 		m.tip = m.tip[0:maxTipSize]
@@ -165,16 +123,16 @@ func (m *monitor) updateTip(
 
 // healthCheck checks the health of the tip when nothing is returned from a
 // scrape enough times.
-func (m *monitor) healthCheck(probe Prober) error {
+func (m *monitor) healthCheck() error {
 	m.blanks++
 	if m.blanks > m.blankThreshold {
 		m.blanks = 0
-		broken, err := m.fixTip(probe)
+		broken, err := m.fixTip()
 		if err != nil {
 			return err
 		}
 		if !broken {
-			m.blankThreshold += blankThreshold
+			m.blankThreshold *= 2
 		}
 	}
 	return nil
@@ -183,8 +141,8 @@ func (m *monitor) healthCheck(probe Prober) error {
 // fixTip checks that the fullname at the front of the tip is still valid (e.g.
 // not deleted).If it isn't, it shaves the tip.fixTip returns whether the tip
 // was broken.
-func (m *monitor) fixTip(probe Prober) (bool, error) {
-	exists, err := probe(m.tip[0])
+func (m *monitor) fixTip() (bool, error) {
+	exists, err := m.lurker.Exists(m.tip[0])
 	if err != nil {
 		return false, err
 	}
--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -1,337 +1,125 @@
 package monitor
 
 import (
-	"fmt"
 	"reflect"
-	"strconv"
 	"testing"
-	"time"
 
-	"github.com/turnage/redditproto"
+	"github.com/turnage/graw/internal/data"
+	"github.com/turnage/graw/internal/reap"
 )
 
-type handler struct {
-	calls        int
-	postCalls    int
-	commentCalls int
-	messageCalls int
+type mockLurker struct {
+	exists bool
 }
 
-func (h *handler) post(post *redditproto.Link) {
-	h.postCalls++
-	h.calls++
+func (m *mockLurker) Listing(_, _ string) (reap.Harvest, error) {
+	return reap.Harvest{}, nil
 }
 
-func (h *handler) comment(comment *redditproto.Comment) {
-	h.commentCalls++
-	h.calls++
+func (m *mockLurker) Exists(_ string) (bool, error) { return m.exists, nil }
+
+func (m *mockLurker) Thread(_ string) (*data.Post, error) {
+	return nil, nil
 }
 
-func (h *handler) message(message *redditproto.Message) {
-	h.messageCalls++
-	h.calls++
+type mockSorter struct {
+	names []string
 }
 
-func float64Pointer(val float64) *float64 {
-	return &val
-}
+func (m *mockSorter) Sort(_ reap.Harvest) []string { return m.names }
 
-func stringPointer(val string) *string {
-	return &val
-}
-
-func MockScraper(
-	links []*redditproto.Link,
-	comments []*redditproto.Comment,
-	messages []*redditproto.Message,
-	err error,
-) Scraper {
-	return func(id, tip string, limit int) (
-		[]*redditproto.Link,
-		[]*redditproto.Comment,
-		[]*redditproto.Message,
-		error,
-	) {
-		return links, comments, messages, err
-	}
-}
-
-func TestBaseFromPath(t *testing.T) {
-	han := &handler{}
-	mon, err := baseFromPath(
-		MockScraper(
-			[]*redditproto.Link{
-				&redditproto.Link{
-					Name: stringPointer("name"),
-				},
-			},
-			nil, nil, nil,
-		),
-		"/r/self",
-		nil,
-		han.comment,
-		nil,
-	)
+func TestNew(t *testing.T) {
+	m, err := New(Config{Lurker: &mockLurker{}, Sorter: &mockSorter{}})
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("error creating monitor: %v", err)
+	}
+	impl := m.(*monitor)
+	if len(impl.tip) < 0 || impl.tip[0] != "" {
+		t.Errorf("tip wrongly initialized; got %v", impl.tip)
 	}
 
-	b := mon.(*base)
-	if b.handleComment == nil {
-		t.Errorf("wanted comment handler set")
+	names := []string{"1", "2"}
+	m, err = New(Config{Lurker: &mockLurker{}, Sorter: &mockSorter{names}})
+	if err != nil {
+		t.Errorf("error creating monitor: %v", err)
 	}
-	if b.path != "/r/self" {
-		t.Errorf("got %s; wanted /r/self", b.path)
-	}
-	mon, err = baseFromPath(
-		MockScraper(
-			[]*redditproto.Link{
-				&redditproto.Link{
-					Name: stringPointer("name"),
-				},
-			}, nil, nil, nil,
-		),
-		"/r/self",
-		nil,
-		nil,
-		nil,
-	)
-	if err == nil {
-		t.Errorf("wanted error if no handlers are provided")
-	}
-}
-
-func TestMerge(t *testing.T) {
-	things := merge(
-		[]*redditproto.Link{
-			&redditproto.Link{
-				CreatedUtc: float64Pointer(2),
-				Name:       stringPointer("two"),
-			},
-			&redditproto.Link{
-				CreatedUtc: float64Pointer(1),
-				Name:       stringPointer("one"),
-			},
-		},
-		[]*redditproto.Comment{
-			&redditproto.Comment{
-				CreatedUtc: float64Pointer(3),
-				Name:       stringPointer("three"),
-			},
-			&redditproto.Comment{
-				CreatedUtc: float64Pointer(0),
-				Name:       stringPointer("zero"),
-			},
-		},
-		[]*redditproto.Message{
-			&redditproto.Message{
-				CreatedUtc: float64Pointer(5),
-				Name:       stringPointer("five"),
-			},
-			&redditproto.Message{
-				CreatedUtc: float64Pointer(4),
-				Name:       stringPointer("four"),
-			},
-		},
-	)
-
-	if len(things) != 6 {
-		t.Fatalf("got %d things; wanted 6", len(things))
-	}
-
-	if things[0].GetName() != "five" {
-		t.Errorf("got %s; wanted five", things[0].GetName())
-	}
-
-	if things[1].GetName() != "four" {
-		t.Errorf("got %s; wanted four", things[1].GetName())
-	}
-
-	if things[2].GetName() != "three" {
-		t.Errorf("got %s; wanted three", things[2].GetName())
-	}
-
-	if things[3].GetName() != "two" {
-		t.Errorf("got %s; wanted two", things[3].GetName())
-	}
-
-	if things[4].GetName() != "one" {
-		t.Errorf("got %s; wanted one", things[4].GetName())
-	}
-
-	if things[5].GetName() != "zero" {
-		t.Errorf("got %s; wanted zero", things[5].GetName())
+	impl = m.(*monitor)
+	if len(impl.tip) != len(names) || !reflect.DeepEqual(names, impl.tip) {
+		t.Errorf("tip wrongly filled; got %v", impl.tip)
 	}
 }
 
 func TestShaveTip(t *testing.T) {
-	b := &base{
-		tip: []string{"1", "2"},
-	}
-	b.shaveTip()
-	if !reflect.DeepEqual(b.tip, []string{"2"}) {
-		t.Errorf("got %v\n; wanted %v", b.tip, []string{"2"})
-	}
-
-	b = &base{
-		tip: nil,
-	}
-	b.shaveTip()
-	if !reflect.DeepEqual(b.tip, []string{""}) {
-		t.Errorf("got %v\n; wanted %v", b.tip, []string{""})
-	}
-}
-
-func TestFixTip(t *testing.T) {
-	b := &base{
-		tip: []string{"1"},
+	m := &monitor{
+		blanks:         1,
+		blankThreshold: 1,
+		tip:            []string{"1", "2"},
+		lurker:         &mockLurker{},
+		sorter:         &mockSorter{},
 	}
 
-	broken, err := b.fixTip(
-		func(id string) (bool, error) {
-			return false, fmt.Errorf("an error")
-		},
-	)
-	if err == nil {
-		t.Errorf("wanted error propagated from operator error")
-	}
-
-	broken, err = b.fixTip(
-		func(id string) (bool, error) {
-			return false, nil
-		},
-	)
-	if !broken {
-		t.Errorf("got false; wanted true")
-	}
-}
-
-func TestUpdateTip(t *testing.T) {
-	b := &base{}
-	for i := 0; i < maxTipSize; i++ {
-		b.tip = append(b.tip, strconv.Itoa(i))
-	}
-
-	err := b.updateTip(
-		[]*redditproto.Link{
-			&redditproto.Link{
-				CreatedUtc: float64Pointer(2),
-				Name:       stringPointer("two"),
-			},
-			&redditproto.Link{
-				CreatedUtc: float64Pointer(1),
-				Name:       stringPointer("one"),
-			},
-		},
-		nil,
-		nil,
-		nil,
-	)
+	_, err := m.Update()
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf("error in update: %v", err)
 	}
 
-	if len(b.tip) != maxTipSize {
-		t.Fatalf("got size %d tip log; wanted %d", len(b.tip), maxTipSize)
+	if len(m.tip) != 1 || m.tip[0] != "2" {
+		t.Errorf("wanted tip shaved; got %v", m.tip)
 	}
 
-	if b.tip[0] != "two" {
-		t.Errorf("got %s; wanted two", b.tip[0])
-	}
-
-	if b.tip[1] != "one" {
-		t.Errorf("got %s; wanted one", b.tip[1])
-	}
-
-	err = b.updateTip(
-		nil,
-		nil,
-		nil,
-		func(id string) (bool, error) {
-			return false, fmt.Errorf("an error")
-		},
-	)
-	if err == nil {
-		t.Errorf("wanted error propagated from healthCheck")
+	if m.blanks != 0 {
+		t.Errorf("wanted blanks reset; got %d", m.blanks)
 	}
 }
 
-func TestHealthCheck(t *testing.T) {
-	b := &base{
-		blankThreshold: blankThreshold,
-		tip:            []string{""},
-	}
-	err := b.healthCheck(
-		func(id string) (bool, error) {
-			return false, nil
-		},
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if b.blanks != 1 {
-		t.Errorf("got %d blanks; wanted 1", b.blanks)
+func TestStoreTip(t *testing.T) {
+	m := &monitor{
+		blanks:         1,
+		blankThreshold: 1,
+		tip:            []string{"1", "2"},
+		lurker:         &mockLurker{},
+		sorter:         &mockSorter{[]string{"0"}},
 	}
 
-	b.blanks = b.blankThreshold
-	err = b.healthCheck(
-		func(id string) (bool, error) {
-			return true, nil
-		},
-	)
+	_, err := m.Update()
 	if err != nil {
-		t.Fatal(err)
-	}
-	if b.blanks != 0 {
-		t.Errorf("got %d blanks; wanted 0", b.blanks)
-	}
-	if b.blankThreshold <= blankThreshold {
-		t.Errorf("got %d; wanted > %d", b.blankThreshold, blankThreshold)
+		t.Errorf("error in update: %v", err)
 	}
 
-	b.blanks = b.blankThreshold
-	err = b.healthCheck(
-		func(id string) (bool, error) {
-			return false, fmt.Errorf("an error")
-		},
-	)
-	if err == nil {
-		t.Fatalf("wanted error propagated from operator")
+	expected := []string{"0", "1", "2"}
+	if len(m.tip) != 3 || !reflect.DeepEqual(m.tip, expected) {
+		t.Errorf("wanted tip expanded; got %v", m.tip)
+	}
+
+	if m.blanks != 0 {
+		t.Errorf("wanted blanks reset; got %d", m.blanks)
 	}
 }
 
-func TestDispatch(t *testing.T) {
-	hand := &handler{}
-	b := &base{
-		handlePost:    hand.post,
-		handleComment: hand.comment,
-		handleMessage: hand.message,
-	}
-	b.dispatch(
-		[]*redditproto.Link{
-			&redditproto.Link{},
-		},
-		[]*redditproto.Comment{
-			&redditproto.Comment{},
-		},
-		[]*redditproto.Message{
-			&redditproto.Message{},
-		},
-	)
-
-	for i := 0; i < 100 && hand.calls < 3; i++ {
-		time.Sleep(10 * time.Millisecond)
+func TestBackoff(t *testing.T) {
+	m := &monitor{
+		blanks:         1,
+		blankThreshold: 1,
+		tip:            []string{"1", "2"},
+		lurker:         &mockLurker{true},
+		sorter:         &mockSorter{},
 	}
 
-	if hand.postCalls != 1 {
-		t.Errorf("got %d post calls; wanted 1", hand.postCalls)
+	_, err := m.Update()
+	if err != nil {
+		t.Errorf("error in update: %v", err)
 	}
 
-	if hand.commentCalls != 1 {
-		t.Errorf("got %d comment calls; wanted 1", hand.commentCalls)
+	expected := []string{"1", "2"}
+	if len(m.tip) != 2 || !reflect.DeepEqual(m.tip, expected) {
+		t.Errorf("wanted tip expanded; got %v", m.tip)
 	}
 
-	if hand.messageCalls != 1 {
-		t.Errorf("got %d message calls; wanted 1", hand.messageCalls)
+	if m.blanks != 0 {
+		t.Errorf("wanted blanks reset; got %d", m.blanks)
+	}
+
+	if m.blankThreshold != 2 {
+		t.Errorf("wanted threshold scaled; got %d", m.blankThreshold)
 	}
 }
