441c38c62787bb99dff67597b571a9d8e2147624
Payton Turnage <paytonturnage@gmail.com>
2016-11-06 19:58:53 -0800

    Define inbox handler interface.
    Former-commit-id: b728cb1264d8c38ab32c86a9c1f5c9945cc96f4c

--- botfaces.go
+++ botfaces.go
@@ -17,3 +17,19 @@ type UserHandler interface {
 	// UserComment handles a new comment made by a watched user.
 	UserComment(c *Comment) error
 }
+
+// InboxHandler handles events from a Reddit account inbox.
+// All methods of this interface must be goroutine safe. An error returned from
+// any method of this interface will stop the graw engine.
+type InboxHandler interface {
+	// Message handles a received private message to the bot's inbox.
+	Message(m *Message) error
+	// PostReply handles a reply to a bot's post received in its inbox.
+	PostReply(m *Message) error
+	// CommentReply handles a reply to a bot's comment received in its
+	// inbox.
+	CommentReply(m *Message) error
+	// Mention handles a mention of the bot's username received in its
+	// inbox.
+	Mention(m *Message) error
+}
