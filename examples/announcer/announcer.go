20136c173b2dd29c2278b22cfedc4fc72963235e
Payton Turnage <paytonturnage@gmail.com>
2016-11-06 18:29:14 -0800

    Implement example bots.
    Former-commit-id: 6eab8b6aaa42d3fe8e0a3b85eb31b778ceffd9d7

---
+++ examples/announcer/announcer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/turnage/graw"
+)
+
+const usage = "Usage of announcer: ./announcer [subreddit] [subreddit] ...\n"
+
+const agent = "graw:announcer-demo-bot:1.0.0 by /u/roxven"
+
+func init() {
+	flag.Usage = func() { fmt.Fprintf(os.Stderr, usage) }
+}
+
+type announcer struct{}
+
+func (a *announcer) Post(p *graw.Post) error {
+	fmt.Printf("[%s]: \"%s\"\n", p.Subreddit, p.Title)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+	}
+
+	fmt.Printf(
+		"Error: %v\n", graw.Run(
+			graw.Config{
+				Agent:      agent,
+				Subreddits: flag.Args(),
+			},
+			&announcer{},
+		),
+	)
+}
---
+++ examples/stalker/stalker.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/turnage/graw"
+)
+
+const usage = "Usage of stalker: ./stalker [user] [user] ...\n"
+
+const agent = "graw:stalker-demo-bot:1.0.0 by /u/roxven"
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usage)
+		os.Exit(0)
+	}
+}
+
+type stalker struct{}
+
+func (a *stalker) UserPost(p *graw.Post) error {
+	fmt.Printf(
+		"[%s in %s]: Posted \"%s\"\n",
+		p.Author, p.Subreddit, p.Title,
+	)
+	return nil
+}
+
+func (a *stalker) UserComment(c *graw.Comment) error {
+	fmt.Printf(
+		"[%s in %s]: Commented on \"%s\"\n",
+		c.Author, c.Subreddit, c.LinkTitle,
+	)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+	}
+
+	fmt.Printf(
+		"Error: %v\n", graw.Run(
+			graw.Config{
+				Agent: agent,
+				Users: flag.Args(),
+			},
+			&stalker{},
+		),
+	)
+}
