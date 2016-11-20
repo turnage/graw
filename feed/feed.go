9620c0419f9bcdf83f0886aa9e941ae099dbf707
Payton Turnage <paytonturnage@gmail.com>
2016-11-19 20:43:10 -0500

    Test bot implementation.

---
+++ feed/feed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/turnage/graw"
+	"github.com/turnage/graw/reddit"
+)
+
+var (
+	subreddit = flag.String("subreddit", "all", "subreddit to watch")
+	rate      = flag.Duration("rate", time.Second, "interval between updates")
+)
+
+type announcer struct{}
+
+func (a *announcer) Post(p *reddit.Post) error {
+	fmt.Printf("[%s]: %s\n", p.Author, p.Title)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	cfg := graw.Config{
+		Subreddits: []string{*subreddit},
+		Logger:     log.New(os.Stderr, "", log.LstdFlags),
+	}
+	if script, err := reddit.NewScript(
+		"graw:feed demo bot:0.5.1 by /u/roxven",
+		*rate,
+	); err != nil {
+		fmt.Printf("Failed to create reddit script: %v\n", err)
+	} else if err := graw.Scan(&announcer{}, script, cfg); err != nil {
+		fmt.Printf("graw run failed: %v\n", err)
+	}
+
+}
